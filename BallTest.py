88073a7960193141a17028ad3ee265090efc515d
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-16 18:00:38 -0400

    Updated from Brython Server: 4/16/2016 6:00:37 PM

--- BallTest.py
+++ BallTest.py
@@ -7,11 +7,11 @@ black = Color(0x000000, 1.0)
 
 noline = LineStyle(0.0, black)
 
-Floor = Sprite(RectangleAsset(10, 10, noline, black))
+Floor = RectangleAsset(SCREEN_WIDTH, 10, noline, black)
 
 class Ball(Sprite):
 
-  asset = CircleAsset(10, noline, black)
+  asset = CircleAsset(30, noline, black)
   
   def __init__(self, position):
     super().__init__(Ball.asset, position)
@@ -21,6 +21,6 @@ class BallBounce(App):
   def __init__(self):
     super().__init__()
     Ball((SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
-    Floor((0,SCREEN_HEIGHT))
+    Sprite(Floor,(0,SCREEN_HEIGHT))
     
 BallBounce().run()
