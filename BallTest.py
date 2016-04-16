17558701319fd0cd8ca2fcf1e3481470d27cfca9
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-16 17:56:52 -0400

    Create BallTest.py

---
+++ BallTest.py
@@ -0,0 +1,26 @@
+from ggame import App, Sprite, CircleAsset, RectangleAsset, Color, LineStyle
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+black = Color(0x000000, 1.0)
+
+noline = LineStyle(black, 0.0)
+
+Floor = Sprite(RectangleAsset(10, 10, noline, black))
+
+class Ball(Sprite):
+
+  asset = CircleAsset(10, noline, black)
+  
+  def __init__(self, position):
+    super().__init__(Ball.asset, positioin)
+
+class BallBounce(App):
+
+  def __init__(self):
+    super().__init__()
+    Ball((SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
+    Floor((0,SCREEN_HEIGHT))
+    
+BallBounce().run()
