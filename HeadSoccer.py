e09345d19f5927984700f60a9268d2ad0faf8a61
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-19 14:38:01 -0400

    Updated from Brython Server: 4/19/2016 2:38:00 PM

--- HeadSoccer.py
+++ HeadSoccer.py
@@ -15,7 +15,7 @@ class PhysicsObject(Sprite):
   
     def __init__(self, asset, position):
         super().__init__(asset, position)
-        self.velocity = (0,0)
+        self.velocity = [0,0]
         
     def step(self):
         self.x += self.velocity[0]
@@ -26,19 +26,36 @@ class Player(PhysicsObject):
     asset = CircleAsset(30, noline, black)
     
     def __init__(self, position):
-        super().__init__(position)
+        super().__init__(Player.asset, position)
         self.mag = 1
         self.speed = 5
+        self.jumpForce = 15
+        self.mass = 5
         HeadSoccer.listenKeyEvent('keydown', 'd', self.right)
         HeadSoccer.listenKeyEvent('keydown', 'a', self.left)
-        HeadSoccer.listenKeyEvent('keyup', 'd', self.left)
-        HeadSoccer.listenKeyEvent('keyup', 'a', self.right)
+        HeadSoccer.listenKeyEvent('keyup', 'd', self.stop)
+        HeadSoccer.listenKeyEvent('keyup', 'a', self.stop)
+        HeadSoccer.listenKeyEvent('keydown', 'w', self.jump)
         
     def right(self, event):
-        self.velocity[0] += self.speed
+        self.velocity[0] = self.speed
         
     def left(self, event):
-        self.velocity[1] -= self.speed
+        self.velocity[0] = -self.speed
+        
+    def stop(self, event):
+        self.velocity[0] = 0
+        
+    def jump(self, event):
+        if self.y == SCREEN_HEIGHT:
+            self.velocity[1] = -self.jumpForce
+            
+    def step(self):
+        super().step()
+        if self.y < SCREEN_HEIGHT:
+            self.velocity[1] += GRAVITY
+        elif self.y == SCREEN_HEIGHT:
+            self.velocity[1] = 0
         
 class Ball(PhysicsObject):
     
@@ -47,6 +64,7 @@ class Ball(PhysicsObject):
     def __init__(self, position):
         super().__init__(Ball.asset, position)
         self.mag = 1
+        self.mass = 1
         HeadSoccer.listenKeyEvent('keydown', 'right arrow', self.right)
         HeadSoccer.listenKeyEvent('keydown', 'left arrow', self.left)
         
@@ -58,21 +76,30 @@ class Ball(PhysicsObject):
         
     def step(self):
         super().step()
-        if self.y >= SCREEN_HEIGHT-35:
+        if self.y >= SCREEN_HEIGHT-30:
             self.velocity[1] *= -1
             self.velocity[1] -= GRAVITY
         self.velocity[1] += GRAVITY
+        if len(self.collidingWithSprites(Player)) > 0:
+            for x in self.collidingWithSprites(Player):
+                self.velocity[0] += x.mass*x.velocity[0]/self.mass
+                self.velocity[1] += x.mass*x.velocity[1]/self.mass
 
 class HeadSoccer(App):
 
     def __init__(self):
         super().__init__()
-        #Ball((SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
+        Ball((SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
         Sprite(Floor,(0,SCREEN_HEIGHT))
-        Player((SCREEN_WIDTH/2,SCREEN_HEIGHT-5))
+        Player((SCREEN_WIDTH/2,SCREEN_HEIGHT))
+        
+    def classStep(self, sclass):
+        for x in self.getSpritesbyClass(sclass):
+            x.step()
         
     def step(self):
-        for x in self.getSpritesbyClass(Ball):
-            x.step()
+        self.classStep(Ball)
+        self.classStep(Player)
+        
     
 HeadSoccer().run()
