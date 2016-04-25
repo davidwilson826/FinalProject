26808e0bfb280525ea342574fef877df02bd85ef
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-25 14:09:46 -0400

    Update CollisionTest.py

--- CollisionTest.py
+++ CollisionTest.py
@@ -1,1 +1,14 @@
+from ggame import App, Sprite, CircleAsset, RectangleAsset, Color, LineStyle, TextAsset
 
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
+
+noline = LineStyle(0.0, black)
+
+def classDestroy(sclass):
+    while len(HeadSoccer.getSpritesbyClass(sclass)) > 0:
+        for x in HeadSoccer.getSpritesbyClass(sclass):
+            x.destroy()
