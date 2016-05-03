19bc65cd87965b8c85f03b0ef1b935412dcf2a1a
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-05-03 14:12:18 -0400

    Updated from Brython Server: 5/3/2016 2:12:17 PM

--- CenterTest.py
+++ CenterTest.py
@@ -2,7 +2,7 @@ from ggame import App, Sprite, CircleAsset, Color, LineStyle
 
 black = Color(0x000000, 1.0)
 
-noline = LineStyle(black, 0.0)
+noline = LineStyle(0.0, black)
 
 class Thing(Sprite):
     
@@ -17,3 +17,5 @@ class CenterTest(App):
     def __init__(self):
         super().__init__()
         Thing((0,0))
+        
+CenterTest().run()
