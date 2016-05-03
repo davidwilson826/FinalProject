72e4d7528f46fc8fe74d950e91ae30d4d1f351b4
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-05-03 14:11:32 -0400

    Updated from Brython Server: 5/3/2016 2:11:32 PM

--- CenterTest.py
+++ CenterTest.py
@@ -2,4 +2,18 @@ from ggame import App, Sprite, CircleAsset, Color, LineStyle
 
 black = Color(0x000000, 1.0)
 
+noline = LineStyle(black, 0.0)
+
 class Thing(Sprite):
+    
+    asset = CircleAsset(50, noline, black)
+    
+    def __init__(self, position):
+        super().__init__(Thing.asset, position)
+        self.fxcenter = self.fycenter = 0.5
+        
+class CenterTest(App):
+    
+    def __init__(self):
+        super().__init__()
+        Thing((0,0))
