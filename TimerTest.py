631f317687124755c5d6f60620546e70e3b41a1b
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-05-03 14:40:14 -0400

    Updated from Brython Server: 5/3/2016 2:40:14 PM

--- TimerTest.py
+++ TimerTest.py
@@ -1,1 +1,25 @@
 from ggame import App, Sprite, TextAsset
+from time import time
+
+class TimeText(Sprite):
+    
+    def __init__(self, asset, position):
+        super().__init__(asset, position)
+        
+class Test(App):
+    
+    def __init__(self):
+        super().__init__()
+        self.start = time()
+        self.elapsed = 0
+        self.dispTime()
+        
+    def dispTime(self):
+        self.elapsed = time()-self.start
+        TimeText(TextAsset(str(self.elapsed//60)+':'+str(int(self.elapsed%60))))
+        
+    def step(self):
+        self.getSpritesbyClass(TimeText)[0].destroy()
+        self.dispTime()
+        
+Test().run()
