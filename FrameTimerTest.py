c52a500b52d162bbea664109fc29182a204ae42f
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-05-04 13:25:26 -0400

    Updated from Brython Server: 5/4/2016 1:25:26 PM

--- FrameTimerTest.py
+++ FrameTimerTest.py
@@ -1,3 +1,4 @@
+'''
 from ggame import App
 from time import time
 
@@ -13,5 +14,23 @@ class Timer(App):
         self.frameTime[1] = time()
         self.deltaTime = self.frameTime[1]-self.frameTime[0]
         print(round(self.deltaTime,10))
+        
+Timer().run()
+'''
+
+from ggame import App
+from time import time
+
+class Timer(App):
+    
+    def __init__(self):
+        super().__init__()
+        self.frameTime = time()
+        self.deltaTime = 0
+        
+    def step(self):
+        self.deltaTime = time()-self.frameTime
+        print(round(self.deltaTime,10))
+        self.frameTime = time()
         
 Timer().run()
