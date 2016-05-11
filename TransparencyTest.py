7531c9a5db472f187d7b84072837a77833ea23e3
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-05-11 13:59:27 -0400

    Updated from Brython Server: 5/11/2016 1:59:27 PM

--- TransparencyTest.py
+++ TransparencyTest.py
@@ -1,1 +1,16 @@
+from ggame import App
 
+class Transparency(App):
+    
+    def __init__(self):
+        super().__init__()
+        self.direction = 0
+        self.transparency = 1
+        
+    def step(self):
+        if self.transparency == 1:
+            self.direction = -0.1
+        elif self.transparency == 0:
+            self.direction = 0.1
+        self.transparency += self.direction
+        print(self.transparency)
