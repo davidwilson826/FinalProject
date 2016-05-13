ec933f56f8e88bccd2a9838977af600486d68879
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-05-13 14:35:41 -0400

    Updated from Brython Server: 5/13/2016 2:35:41 PM

--- TransparencyTest.py
+++ TransparencyTest.py
@@ -9,16 +9,19 @@ class Transparency(App):
         super().__init__()
         self.direction = 0
         self.transparency = 1
+        TextSprite(TextAsset('Click to Continue', fill=Color(0x000000, self.transparency), 
+        width=500), (200,200))
         
     def step(self):
-        TextSprite(TextAsset('Click to Continue', fill=Color(0x000000, self.transparency)))
+        self.getSpritesbyClass(TextSprite)[0].destroy()
+        TextSprite(TextAsset('Click to Continue', fill=Color(0x000000, self.transparency), 
+        width=500), (200,200))
         if self.transparency == 1:
             self.direction = -0.1
         elif self.transparency == 0:
             self.direction = 0.1
         self.transparency += self.direction
         self.transparency = round(self.transparency, 1)
-        print(self.transparency)
-        self.getSpritesbyClass(TextSprite)[0].destroy()
+        #print(self.transparency)
         
 Transparency().run()
