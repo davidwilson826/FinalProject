be63c98d7263670973c75f2f4a72ec3f7627120c
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-05-23 14:21:24 -0400

    Updated from Brython Server: 5/23/2016 2:21:24 PM

--- CollisionSensor.py
+++ CollisionSensor.py
@@ -1,1 +1,23 @@
+from ggame import App, Sprite, Color, LineStyle, RectangleAsset, CircleAsset
 
+black = Color(0x000000, 1.0)
+blue = Color(0x0000ff, 1.0)
+
+noline = LineStyle(black, 0.0)
+
+class Ball(Sprite):
+    
+    r = 30
+    asset = CircleAsset(r, noline, black)
+    
+    def __init__(self, position):
+        super().__init__(Ball.asset, position)
+        self.fxcenter = self.fycenter = 0.5
+        
+class Collisions(App):
+    
+    def __init__(self):
+        super().__init__()
+        Ball((0,0))
+        
+Collisions().run()
