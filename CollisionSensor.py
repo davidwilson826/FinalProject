9559f0fd330038e92c2b0dbdafbc59f410ccfd9f
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-05-23 14:25:10 -0400

    Updated from Brython Server: 5/23/2016 2:25:10 PM

--- CollisionSensor.py
+++ CollisionSensor.py
@@ -3,21 +3,39 @@ from ggame import App, Sprite, Color, LineStyle, RectangleAsset, CircleAsset
 black = Color(0x000000, 1.0)
 blue = Color(0x0000ff, 1.0)
 
-noline = LineStyle(black, 0.0)
+noline = LineStyle(0.0, black)
 
 class Ball(Sprite):
     
     r = 30
-    asset = CircleAsset(r, noline, black)
+    asset = CircleAsset(r, noline, blue)
     
     def __init__(self, position):
         super().__init__(Ball.asset, position)
         self.fxcenter = self.fycenter = 0.5
+        self.velocity = (0,0)
+        Collisions.listenKeyEvent('keydown', 'left arrow', self.left)
+        Collisions.listenKeyEvent('keydown', 'right arrow', self.right)
+        Collisions.listenKeyEvent('keydown', 'right arrow', self.right)
+        Collisions.listenKeyEvent('keydown', 'right arrow', self.left)
+        
+    def left(self, event):
+        self.velocity[0] -= 5
+        
+    def right(self, event):
+        self.velocity[0] += 5
+        
+    def step(self):
+        self.x += self.velocity[0]
+        self.y += self.velocity[1]
         
 class Collisions(App):
     
     def __init__(self):
         super().__init__()
-        Ball((0,0))
+        Ball((200,200))
+        
+    def step(self):
+        self.getSpritesbyClass(Ball)[0].step()
         
 Collisions().run()
