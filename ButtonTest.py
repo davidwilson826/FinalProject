fdf6b31d75104cf27cf2fa0edd9911093b335f8d
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-05-25 13:31:03 -0400

    Updated from Brython Server: 5/25/2016 1:31:03 PM

--- ButtonTest.py
+++ ButtonTest.py
@@ -7,18 +7,18 @@ black = Color(0x000000, 1.0)
 
 noLine = LineStyle(0.0, black)
 
+class Border(Sprite):
+    width = 5
+
 class Button(Sprite):
-    
-    def __init__(self, asset, position):
-        super().__init__(asset, position)
-        self.fxcenter = self.fycenter = 0.5
+    pass
         
 class Test(App):
     
     def __init__(self):
         super().__init__()
-        Button(RectangleAsset(SCREEN_WIDTH, 5, noLine, black), (0, SCREEN_HEIGHT-2.5))
-        Button(RectangleAsset(5, SCREEN_HEIGHT, noLine, black), (SCREEN_WIDTH-2.5, 0))
+        Border(RectangleAsset(SCREEN_WIDTH, Border.width, noLine, black), (0, SCREEN_HEIGHT-(Border.width/2)))
+        Border(RectangleAsset(Border.width, SCREEN_HEIGHT, noLine, black), (SCREEN_WIDTH-(Border.width/2), 0))
         for x in range(9):
             width = 0.2*SCREEN_WIDTH
             height = 0.2*SCREEN_HEIGHT
